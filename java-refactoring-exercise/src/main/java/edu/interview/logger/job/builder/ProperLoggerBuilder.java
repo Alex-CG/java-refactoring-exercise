515949adb8b13910f145b4e7ee99f5bb5a095219
Alex-CG <a.contrerasgranados@gmail.com>
2018-09-02 16:54:26 -0500

    IllegalArgumentException implementation

--- java-refactoring-exercise/src/main/java/edu/interview/logger/job/builder/ProperLoggerBuilder.java
+++ java-refactoring-exercise/src/main/java/edu/interview/logger/job/builder/ProperLoggerBuilder.java
@@ -7,6 +7,7 @@ import edu.interview.logger.job.enums.LogType;
 public class ProperLoggerBuilder implements LoggerBuilder {
 
     private FormalJobLogger logger;
+    private String NULL_NOT_ALLOWED = "Null argument is not allowed!";
 
     public ProperLoggerBuilder() {
         logger = new FormalJobLogger();
@@ -14,12 +15,14 @@ public class ProperLoggerBuilder implements LoggerBuilder {
 
     @Override
     public LoggerBuilder withLogTo(LogDestination destination) {
+        if (destination == null) throw new IllegalArgumentException(NULL_NOT_ALLOWED);
         logger.enableLogTo(destination, Boolean.TRUE);
         return this;
     }
 
     @Override
     public LoggerBuilder withLogType(LogType type) {
+        if (type == null) throw new IllegalArgumentException(NULL_NOT_ALLOWED);
         logger.enableLogType(type, Boolean.TRUE);
         return this;
     }
